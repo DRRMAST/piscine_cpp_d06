f5b17ecb2b544736df6e495c793ea01feec70b1c
DRRMAST <heztzxy@yeah.net>
2018-07-09 23:21:43 +0800

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "Peon.hh"
+#include "Sorcerer.hh"
+#include "Victim.hh"
+
+int main()
+{
+	Sorcerer robert("Robert", "the Magnificent");
+	Victim jim("Jimmy");
+	Peon joe("Joe");
+	std::cout << robert << jim << joe;
+	robert.polymorph(jim);
+	robert.polymorph(joe);
+	return 0;
+}
